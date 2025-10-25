2d430ef6918ecddde2146d345e0441b77f345ea1
pongpatapee <pongpatapee1021@gmail.com>
2025-10-25 19:15:39 -0400

    init keymaps and opt

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,20 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+local map = vim.keymap.set
+
+-- better escape
+map("i", "jk", "<ESC>", { noremap = true, silent = true })
+
+-- Dedicated copy paste to and from clipboard so I don't have to use vim.opt.clipboard = "unnamedplus" -- Sync with system clipboard
+map({ "n", "v" }, "<leader>p", [["+p]], { desc = "clipboard paste" })
+map({ "n", "v" }, "<leader>y", [["+y]], { desc = "clipboard yank" })
+
+-- allow for Ctrl + Backspace for deleting words
+map("i", "<C-h>", "<C-w>", { noremap = true, silent = true }) -- Can't bind <C-BS> directly, this is the work around
+
+map("n", "<leader>-", "<C-W>s", { desc = "Split Window Below", remap = true })
+map("n", "<leader>_", "<C-W>s", { desc = "Split Window Below", remap = true })
+map("n", "<leader>|", "<C-W>v", { desc = "Split Window Right", remap = true })
+map("n", "<leader>\\", "<C-W>v", { desc = "Split Window Right", remap = true })
